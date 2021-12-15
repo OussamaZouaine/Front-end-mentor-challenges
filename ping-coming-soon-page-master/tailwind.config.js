701d31ff93b7aba754e84419da7e54fdea5c7017
OussamaZouaine <oussamazouaine@gmail.com>
2021-12-15 23:39:19 +0100

    initial modification

---
+++ ping-coming-soon-page-master/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    content: ['./index.html'],
+    theme: {
+        extend: {
+            colors: {
+                primary: 'hsl(223, 87%, 63%)',
+                'pale-blue': 'hsl(223, 100%, 88%)',
+                'light-red': 'hsl(354, 100%, 66%)',
+                grey: 'hsl(0, 0%, 59%)',
+                'very-dark-blue': 'hsl(209, 33%, 12%)',
+            },
+            fontFamily: {
+                'libre-franklin': ['Libre Franklin', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
+};
